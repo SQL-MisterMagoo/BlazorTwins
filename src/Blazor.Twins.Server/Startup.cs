caac2a9c7d3115c441b24e3d665ce54c2d58ed25
Mister Magoo <mister.magoo+githubsql@gmail.com>
2019-06-15 01:25:09 +0100

    Minor tweaks to make consistent with current templates

--- src/Blazor.Twins.Server/Startup.cs
+++ src/Blazor.Twins.Server/Startup.cs
@@ -2,6 +2,7 @@ using Blazor.Twins.Core.Services;
 using Blazor.Twins.Server.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -9,6 +10,13 @@ namespace Blazor.Twins.Server
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -31,6 +39,7 @@ namespace Blazor.Twins.Server
                 app.UseHsts();
             }
 
+            //Feel free to uncomment this - I just can't seem to get https to work on anything except Edge
             //app.UseHttpsRedirection();
 
             app.UseStaticFiles();
